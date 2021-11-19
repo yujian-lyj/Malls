28357fdccc99cea656852ddda70e6e732239ddd9
yujian-lyj <980203700@qq.com>
2021-11-19 10:22:33 +0800

    update lazyload

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import store from './srote'
 import App from './App.vue'
 import router from './router'
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
@@ -11,6 +14,12 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
     //安装toast
 Vue.use(toast)
+    //解决移动端300ms延迟
+FastClick.attach(document.body)
+    //图片懒加载
+Vue.use(VueLazyLoad, {
+    loading: require('./assets/img/common/placeholder1.png')
+})
 
 new Vue({
     render: h => h(App),
